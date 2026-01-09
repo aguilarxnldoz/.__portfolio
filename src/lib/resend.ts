5cc7be31c6a95dc422873027641883b2a0bf1be5
hateNaldoz <aguilarnald@gmail.com>
2026-01-08 21:06:13 -0800

    debug: resend & redis

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,2 +1,7 @@
 import {Resend} from "resend";
-export const resend = new Resend(process.env.RESEND_API_KEY);
+
+if (!process.env.RESEND_API_KEY) {
+    console.warn("Missing RESEND_API_KEY environment variable");
+}
+
+export const resend = new Resend(process.env.RESEND_API_KEY || "re_123456789");
