74c296bac41e8d7eb7f38a306c1f6a2d748c6f3d
hateNaldoz <aguilarnald@gmail.com>
2026-03-12 22:20:12 -0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import PageWrapper from "@/components/PageWrapper";
 import MovingBars from "@/components/MovingBars";
 import TechStackContainer from "@/components/TechStack";
@@ -9,11 +9,11 @@ import ExperienceContainer from "@/components/Experience";
 import EducationContainer from "@/components/EducationContainer";
 
 export default function Home() {
-    return (
-        <>
-            <PageWrapper>
-                <div className="md:flex md:flex-row md:justify-center md:gap-20 md:my-50">
-                    {/* <div className="flex flex-col-reverse items-center gap-5">
+	return (
+		<>
+			<PageWrapper>
+				<div className="md:flex md:flex-row md:justify-center md:gap-20 md:my-50">
+					{/* <div className="flex flex-col-reverse items-center gap-5">
                         <Image
                             className="rounded-full sm:rounded-2xl"
                             src={"/naldouche.jpg"}
@@ -22,38 +22,38 @@ export default function Home() {
                             height={300}
                         />
                     </div> */}
-                    <div>
-                        <div className="text-crimson text-center">
-                            <h1 className="text-2xl text-dark mb-6 sm:mb-3 mt-2.5 w-full">Nald O. Aguilar</h1>
-                            <div className="flex flex-row sm:space-x-5 ">
-                                <Ghost className="w-14 sm:w-10 h-10" />
-                                <h2>Full-stack Web Developer</h2>
-                                <Ghost className="w-14 sm:w-10 h-10" />
-                            </div>
-                        </div>
-                        <div className="text-center sm:w-100 mx-auto bg-platinum p-3 mt-5 rounded-2xl shadow-md">
-                            <p>I am a 2nd year student in Computer Science with an unhealthy addiction to tech. Using modern technologies, I build, deploy, and scale applications to industry standards.</p>
-                        </div>
-                    </div>
-                </div>
-            </PageWrapper>
+					<div>
+						<div className="text-crimson text-center">
+							<h1 className="text-2xl text-dark mb-6 sm:mb-3 mt-2.5 w-full">Nald O. Aguilar</h1>
+							<div className="flex flex-row sm:space-x-5 ">
+								<Ghost className="w-14 sm:w-10 h-10" />
+								<h2>Full-stack Web Developer</h2>
+								<Ghost className="w-14 sm:w-10 h-10" />
+							</div>
+						</div>
+						<div className="text-center sm:w-100 mx-auto bg-platinum p-3 mt-5 rounded-2xl shadow-md">
+							<p>I am a 2nd year student in Computer Science with an unhealthy addiction to tech. Using modern technologies, I build, deploy, and scale applications to industry standards.</p>
+						</div>
+					</div>
+				</div>
+			</PageWrapper>
 
-            <MovingBars />
+			<MovingBars />
 
-            <PageWrapper>
-                <div className="flex flex-col items-center mt-20 md:mt-35">
-                    <section
-                        id="experience-section"
-                        className="w-full items-center m-auto my-10"
-                    >
-                        <div className="flex flex-row gap-5 sm:gap-10">
-                            <Ghost className="w-10 h-10 bottom-2.5" />
-                            <h2 id="experience">Experience</h2>
-                        </div>
-                        <ExperienceContainer />
-                    </section>
+			<PageWrapper>
+				<div className="flex flex-col items-center mt-20 md:mt-35">
+					<section
+						id="experience-section"
+						className="w-full items-center m-auto my-10"
+					>
+						<div className="flex flex-row gap-5 sm:gap-10">
+							<Ghost className="w-10 h-10 bottom-2.5" />
+							<h2 id="experience">Experience</h2>
+						</div>
+						<ExperienceContainer />
+					</section>
 
-                    {/* <section
+					{/* <section
                         id="case-study-section"
                         className="w-full items-center m-auto my-10"
                     >
@@ -64,52 +64,52 @@ export default function Home() {
                         </div>
                     </section> */}
 
-                    <section
-                        id="projects-section"
-                        className="w-full items-center m-auto my-10"
-                    >
-                        <div className="flex flex-row gap-5 sm:gap-10">
-                            <Ghost className="w-10 h-10 bottom-2.5" />
-                            <h2 id="projects">Projects</h2>
-                        </div>
+					<section
+						id="projects-section"
+						className="w-full items-center m-auto my-10"
+					>
+						<div className="flex flex-row gap-5 sm:gap-10">
+							<Ghost className="w-10 h-10 bottom-2.5" />
+							<h2 id="projects">Projects</h2>
+						</div>
 
-                        <ProjectsContainer />
-                    </section>
+						<ProjectsContainer />
+					</section>
 
-                    <section
-                        id="techstack-section"
-                        className="w-full items-center m-auto my-10"
-                    >
-                        <div className="flex flex-row gap-5 sm:gap-10 w-auto">
-                            <Ghost className="w-10 h-10 bottom-2.5" />
-                            <h2 id="techstack">Tech Stack</h2>
-                        </div>
-                        <TechStackContainer />
-                    </section>
+					<section
+						id="techstack-section"
+						className="w-full items-center m-auto my-10"
+					>
+						<div className="flex flex-row gap-5 sm:gap-10 w-auto">
+							<Ghost className="w-10 h-10 bottom-2.5" />
+							<h2 id="techstack">Tech Stack</h2>
+						</div>
+						<TechStackContainer />
+					</section>
 
-                    <section
-                        id="education-section"
-                        className="w-full items-center m-auto my-10"
-                    >
-                        <div className="flex flex-row gap-5 sm:gap-10 w-auto">
-                            <Ghost className="w-10 h-10 bottom-2.5" />
-                            <h2 id="techstack">Education</h2>
-                        </div>
-                        <EducationContainer />
-                    </section>
+					<section
+						id="education-section"
+						className="w-full items-center m-auto my-10"
+					>
+						<div className="flex flex-row gap-5 sm:gap-10 w-auto">
+							<Ghost className="w-10 h-10 bottom-2.5" />
+							<h2 id="techstack">Education</h2>
+						</div>
+						<EducationContainer />
+					</section>
 
-                    <section
-                        id="contact-section"
-                        className="w-full items-center m-auto my-10"
-                    >
-                        <div className="flex flex-row gap-5 sm:gap-10">
-                            <Ghost className="w-10 h-10 bottom-2.5" />
-                            <h2 id="contact">Send me a Message!</h2>
-                        </div>
-                        <EmailForm />
-                    </section>
-                </div>
-            </PageWrapper>
-        </>
-    );
+					<section
+						id="contact-section"
+						className="w-full items-center m-auto my-10"
+					>
+						<div className="flex flex-row gap-5 sm:gap-10">
+							<Ghost className="w-10 h-10 bottom-2.5" />
+							<h2 id="contact">Send me a Message!</h2>
+						</div>
+						<EmailForm />
+					</section>
+				</div>
+			</PageWrapper>
+		</>
+	);
 }
