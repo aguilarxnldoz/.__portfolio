367de3b7974364f937efc16d8d5c02a29f55bfde
hateNaldoz <aguilarnald@gmail.com>
2026-04-21 10:51:06 -0700

    fix: experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -10,14 +10,14 @@ export default function ExperienceContainer() {
 					companyName="Vero Ventures - InsurFlow"
 					jobType="internship"
 					role="Software Engineer"
-					workTerm="January 2026 - Present"
+					workTerm="January 2026 - April 2026"
 					description="Developed a build for greenfield insurtech application with AI integration. Specifically built the calculation engine to provide 20,000+ life insurance advisors in the United States a fast way to calculate insurance for clients. Technologies Used: NextJS,  + drizzle orm, shadcn, openai, docker"
 				/>
 				<ExperienceCard
 					companyName="Vince Villanueva Real Estate"
 					jobType="freelance"
 					role="Full-stack Developer"
-					workTerm="March 2026 - April 2026"
+					workTerm="March 2026 - current"
 					description="Built a Full-stack lead-generation website for a Realtor in the Greater Vancouver & Fraser Valley area to increase client-base and improve professional branding. Application integrates automation for emails, and filling Excel sheets "
 				/>
 			</div>
