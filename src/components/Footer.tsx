caf0e5539347d575d9cfbb0946275783bcb2a33f
hateNaldoz <aguilarnald@gmail.com>
2026-04-25 20:01:43 -0700

    fix: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
-    return (
-        <footer className="bg-platinum w-full max-w-full text-center py-1 bottom-0">
-            <p className="font-bold">
-                Copyright &copy; 2025 Nald Aguilar.<span className="block"> All rights reserved</span>
-            </p>
-        </footer>
-    );
+	return (
+		<footer className="bg-platinum w-full max-w-full text-center py-1 bottom-0">
+			<p className="font-bold">
+				Copyright &copy; 2026 Nald Aguilar.<span className="block"> All rights reserved</span>
+			</p>
+		</footer>
+	);
 }
